d3f9f483206010d9ee0faafc89a0373aae1996b4
Muriki G. Yamanaka <mgy20@inf.ufpr.br>
2025-09-04 11:34:28 -0300

    Fix: Outputs from encrypt.py

--- Crypto1/encrypt.py
+++ Crypto1/encrypt.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 """
-Author: Muriki G. Yamanaka
-Description: Encryption algorithm
+ Author: Muriki G. Yamanaka
+ Description: Encryption algorithm
 """
 
 import sys
@@ -10,24 +10,32 @@ import sys
 def sum_digits(digit):
     return sum(int(x) for x in digit)
 
-def vinegere(key):
-    key_vec = list(key)  # List because the class String is immutable
+"""
+ Processes the key as a string
+"""
+def vinegere(key, raw_text, crypted_text):
+    key_vec = list(key)  # List because class String is immutable
     key_size = len(key)
-    print("=== Vinegere", f'Key: {key}', f'Len: {key_size} ===')
+    #print("=== Vinegere", f'Key: {key}', f'Len: {key_size} ===')
 
-    i = 0;
-    for line in sys.stdin:
-        for token in line:
-            if i == key_size:
-                i = 0
-            #print(chr(ord(token)+ord(key_vec[i])))
-            key_vec[i] = token
-            i+=1
+    i = 0; j = 0;
+    for token in raw_text:
+        if i == key_size:
+            i = 0
+        crypted_text.append(chr(ord(token)+ord(key_vec[i])))
+        key_vec[i] = token
+        i+=1; j+=1
 
-def rail_fence(key):
-    key_vec = list(key)  # List because the class String is immutable
-    key_size = len(key)
-    print("=== Rail Fence", f'Key: {key}', f'Len: {key_size} ===')
+"""
+ Processes the key as a number
+ The key represents the number of columns
+"""
+def rail_fence(key, raw_text, crypted_text):
+    print("=== Rail Fence", f'Key: {key} ===')
+    
+    #for i in range(key):
+    #    crypted_text.append(raw_text[i:])
+
 
 
 # === Main
@@ -43,7 +51,8 @@ date = sys.argv[1]
 day = int(date[:2])
 month = int(date[2:4])
 year = int(date[4:8])
-replace_loop = day+month
+#replace_loop = day+month
+replace_loop = sum_digits(date[:4])
 transform_loop = sum_digits(date[4:8])
 
 #print(f'date: {day}')
@@ -52,10 +61,22 @@ transform_loop = sum_digits(date[4:8])
 #print(f'replace: {replace_loop}')
 #print(f'transform: {transform_loop}')
 
-#for _ in range(replace_loop):
-print(f'- Replace loops {replace_loop}')
-vinegere(date[:2])
+# Read input
+raw_text = list(); crypted_text = list()
+for line in sys.stdin:
+    raw_text += line
+print(f'Raw: {raw_text}')
 
+print(f'\n- Replace loops {replace_loop}')
+for _ in range(replace_loop):
+    vinegere(date[:2], raw_text, crypted_text)
+    raw_text = crypted_text.copy()
+    crypted_text.clear()
+
+print(f'Vinegere: {raw_text}')
+
+print(f'\n- Transform loops {transform_loop}')
 #for _ in range(transform_loop):
-print(f'- Replace loops {transform_loop}')
-rail_fence(date[2:4])
+rail_fence(int(date[2:4]), raw_text, crypted_text)
+
+print(f'Rail Fence: {crypted_text}')
